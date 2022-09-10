eb68bb22c6ae7d7ec33b04a7efe451910a2233c7
sergio mendez <sergiomendezgzalez@gmail.com>
2022-09-10 13:54:03 -0600

    guardando el ejemplo de creacion de clases y lista de objetos

--- proyecto/hello.cpp
+++ proyecto/hello.cpp
@@ -1,10 +1,49 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
+
+
+class person
+{
+    int age;
+    string name;
+
+public:
+    person(int age, string name)
+    {
+        this->age = age;
+        this->name = name;
+    }
+    person(){};
+    void show();
+    void insertDatintoPerson();
+};
+vector<person> personas;
+void person::show()
+{
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+}
+
+void person::insertDatintoPerson(){
+    person p(20, "Juan");
+    personas.insert(personas.begin(), p);
+    person p2(30, "Pedro");
+    personas.insert(personas.begin(), p2);
+   
+}
+
+void showPerson(){
+    for (int i = 0; i < personas.size(); i++)
+    {
+        personas[i].show();
+    }
+}
+
 int main()
 {
-cout<<"Hello World: ";
-int a;
-cin>>a;
-cout<<a;
-return 0;
+    person p;
+    p.insertDatintoPerson();
+    showPerson();
+    return 0;
 }
